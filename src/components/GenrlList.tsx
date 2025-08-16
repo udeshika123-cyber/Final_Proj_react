9feee839ee3d9fc85633614ed655308537c89682
udeshika123-cyber <udeshikaprabhashini21@gmail.com>
2025-08-16 16:26:31 +0530

    added spinner to genre list

--- src/components/GenrlList.tsx
+++ src/components/GenrlList.tsx
@@ -1,9 +1,13 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
 import getOptimizedImageUrl from "../services/image-url";
 
 const GenrlList = () => {
-  const { data } = useGenre();
+  const { data, loading, error } = useGenre();
+  if (error) return null;
+
+  if (loading) return <Spinner />; // Optionally handle loading state
+
   return (
     <List.Root listStyle={"none"} padding={0}>
       {data.map((genre) => (
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../../assets/logo1.png"; // or logo1.jpeg if that's the correct name
+import logo from "../../assets/logo1.png";
 
 const NavBar = () => {
   return (
